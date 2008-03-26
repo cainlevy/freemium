ce4a11254fdef6cae99702c864f9a8189fb89582
Lance Ivy <lance@cainlevy.net>
2008-03-26 03:55:45 +0000

    reworking the configuration defaults and the inclusion of the ManualBilling/RecurringBilling modules to cooperate better with a config/initializers/freemium.rb

--- init.rb
+++ init.rb
@@ -1,9 +1,2 @@
 # depends on the Money gem
 require 'money'
-
-# default config. yes, this should happen somewhere else. i'll do that later.
-Freemium.mailer     = SubscriptionMailer
-Freemium.gateway    = Freemium::Gateways::Test.new
-Freemium.days_grace = 3
-Freemium.expired_plan = SubscriptionPlan.find(:first, :conditions => "rate_cents = 0")
-Subscription.send(:include, Freemium.manual_billing ? Freemium::ManualBilling : Freemium::RecurringBilling)
--- lib/freemium.rb
+++ lib/freemium.rb
@@ -1,23 +1,47 @@
 module Freemium
   class << self
-    # Lets you configure which ActionMailer class contains an appropriate
-    # "invoice" method/template. This method should imitate the provided
-    # invoicer: SubscriptionMailer#invoice
-    attr_accessor :mailer
+    # Lets you configure which ActionMailer class contains appropriate
+    # mailings for invoices, expiration warnings, and expiration notices.
+    # You'll probably want to create your own, based on lib/subscription_mailer.rb.
+    attr_writer :mailer
+    def mailer
+      @mailer ||= SubscriptionMailer
+    end
 
     # The gateway of choice. Default gateway is a stubbed testing gateway.
-    attr_accessor :gateway
+    attr_writer :gateway
+    def gateway
+      @gateway ||= Freemium::Gateways::Test.new
+    end
 
-    # Whether or not to enable manual billing. Note that billing will still be
-    # automatic for *you*, but the question is whether Freemium itself should
-    # manually bill subscriptions or rely on the gateway's automated recurring
-    # billing module.
-    attr_accessor :manual_billing
+    # If you want all of the billing to be initiated by Freemium.
+    #
+    # Check whether this option is available for your gateway of choice.
+    # Note that either this or enable_arb_integration must be called for proper setup.
+    def fully_controlled_billing
+      Subscription.send(:include, Freemium::ManualBilling)
+    end
+
+    # If you want the monthly cycles to be handled by your gateway's automated
+    # recurring billing (ARB) system, and Freemium to just keep up-to-date on
+    # transactions.
+    #
+    # Check whether this option is available for your gateway of choice.
+    # Note that either this or fully_controlled_billing must be called for proper setup.
+    def enable_arb_integration
+      Subscription.send(:include, Freemium::RecurringBilling)
+    end
 
     # How many days to keep an account active after it fails to pay.
-    attr_accessor :days_grace
+    attr_writer :days_grace
+    def days_grace
+      @days_grace ||= 3
+    end
 
     # What plan to assign to subscriptions that have expired. May be nil.
-    attr_accessor :expired_plan
+    attr_writer :expired_plan
+    def expired_plan
+      @expired_plan ||= SubscriptionPlan.find(:first, :conditions => "rate_cents = 0")
+    end
   end
 end
